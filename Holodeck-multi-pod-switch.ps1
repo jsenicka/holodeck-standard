ace804d2c3a7e1a6267bee6a8d8ff1dde9b8ab3f
jsenicka <101486373+jsenicka@users.noreply.github.com>
2022-08-13 14:01:44 +0000

    Sample switch creation script

---
+++ Holodeck-multi-pod-switch.ps1
@@ -0,0 +1,13 @@
+﻿Connect-VIServer -Server my-server.home.lab -User root -Password 'ESXiserverPassword'
+New-VirtualSwitch -Name VLC-A -Mtu 9000 
+New-VirtualSwitch -Name VLC-B -Mtu 9000 
+New-VirtualSwitch -Name VLC-C -Mtu 9000 
+New-VirtualSwitch -Name VLC-D -Mtu 9000 
+New-VirtualPortGroup -Name VLC-A-PG -VirtualSwitch VLC-A -VLanId 4095
+New-VirtualPortGroup -Name VLC-B-PG -VirtualSwitch VLC-B -VLanId 4095
+New-VirtualPortGroup -Name VLC-C-PG -VirtualSwitch VLC-C -VLanId 4095
+New-VirtualPortGroup -Name VLC-D-PG -VirtualSwitch VLC-D -VLanId 4095
+Get-VirtualPortGroup -Name VLC-A-PG | Get-SecurityPolicy |  Set-SecurityPolicy     -AllowPromiscuous $true -ForgedTransmits $true -MacChanges $true
+Get-VirtualPortGroup -Name VLC-B-PG | Get-SecurityPolicy |  Set-SecurityPolicy     -AllowPromiscuous $true -ForgedTransmits $true -MacChanges $true
+Get-VirtualPortGroup -Name VLC-C-PG | Get-SecurityPolicy |  Set-SecurityPolicy     -AllowPromiscuous $true -ForgedTransmits $true -MacChanges $true
+Get-VirtualPortGroup -Name VLC-D-PG | Get-SecurityPolicy |  Set-SecurityPolicy     -AllowPromiscuous $true -ForgedTransmits $true -MacChanges $true
