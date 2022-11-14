7ad7729c9f4672b572b1d0f60f5a1f1081b8e53b
jsenicka <101486373+jsenicka@users.noreply.github.com>
2022-11-14 09:17:28 -0600

    Delete VLC-Holo-Site-2 directory

--- VLC-Holo-Site-2/Holo-Build/OC-Generate-Prep.ps1
+++
@@ -1,2 +0,0 @@
-﻿Connect-VIServer -Server vcenter-mgmt.vcf2.sddc.lab -User administrator@vsphere.local -Password VMware123!
-New-VICredentialStoreItem -host Holo-Template -User ocuser -Password VMware123!
